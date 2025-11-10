049826e8346bc6e8e27696e3ad10fda74d2c86c7
alireza.azizi <alireza.azizi@pnr.ir>
2025-11-10 23:34:54 +0330

    consumer got called

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -9,10 +10,17 @@ from app.kafka_manager import kafka_manager
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await kafka_manager.start()
-    print("kafka producer started...")
+    print("kafka producer started")
+    consumer_task = asyncio.create_task(kafka_manager.start_consumer())
+    print("kafka consumer started")
     yield
+    consumer_task.cancel()
+    try:
+        await consumer_task
+    except asyncio.CancelledError:
+        pass
     await kafka_manager.stop()
-    print("kafka producer stopped...")
+    print("kafka producer stopped!!!!!")
 
 
 app = FastAPI(lifespan=lifespan)
