fc69e99025574adbe33a6161f777918b701f9c05
alireza.azizi <alireza.azizi@pnr.ir>
2025-11-10 23:34:13 +0330

    bug(database configuration): mongodb configuration got fixed.

--- app/db.py
+++ app/db.py
@@ -1,9 +1,15 @@
-from pymongo.mongo_client import MongoClient
+from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+import certifi
 from app.config import settings
 
-client = MongoClient(settings.MONGODB_URI, ServerApi=ServerApi('1'))
-db = client[settings.model_config]
+client = MongoClient(
+    settings.MONGODB_URI,
+    server_api=ServerApi('1'),
+    tls=True,
+    tlsCAFile=certifi.where()
+)
+db = client[settings.MONGODB_DB]
 
 def get_collection(name: str):
     return db[name]
