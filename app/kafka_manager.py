71de9be9566ada3c059ae30d48cda98a0e86754e
alireza.azizi <alireza.azizi@pnr.ir>
2025-11-10 23:34:41 +0330

    feat(kafka-consumer):kafka consumer added.

--- app/kafka_manager.py
+++ app/kafka_manager.py
@@ -1,12 +1,12 @@
 import json
 
-from aiokafka import  AIOKafkaProducer
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
 
 from app.config import settings
 
 
 class KafkaManager:
-    def __inti__(self):
+    def __init__(self):
         self.producer = None
         self.consumer = None
 
@@ -23,6 +23,20 @@ class KafkaManager:
 
     async def send_event(self, data: dict):
         await self.producer.send_and_wait(settings.KAFKA_TOPIC, data)
+
+    async def start_consumer(self):
+        consumer = AIOKafkaConsumer(
+            settings.KAFKA_TOPIC,
+            bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVERS,
+            value_deserializer=lambda m: json.loads(m.decode("utf-8")),
+            group_id="test_group",
+        )
+        await consumer.start()
+        try:
+            async for msg in consumer:
+                print("kafka_received:", msg.value)
+        finally:
+            await consumer.stop()
 
 
 kafka_manager = KafkaManager()
